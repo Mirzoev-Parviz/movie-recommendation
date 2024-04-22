982d2f2bd031a6a0939521d106c652b65717dbd8
Mirzoev Parviz <notstivjobs@gmail.com>
2024-04-22 13:27:09 -0600

    deleted unnnecesessary interface

--- internal/services/services.go
+++ internal/services/services.go
@@ -3,13 +3,11 @@ package services
 import "github.com/Mirzoev-Parviz/movie-recommendation/internal/repository"
 
 type Service struct {
-	CSV_Parser
 	RCM
 }
 
 func NewServices(repo *repository.Repository) *Service {
 	return &Service{
-		NewCSV_Parser(),
 		NewRCM_Service(),
 	}
 }
